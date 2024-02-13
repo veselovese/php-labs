b1a78fd38f6aae454974fff584f33e1b2932a9a6
veselovese <motte_vessale@mail.ru>
2024-02-13 11:20:01 +0300

    start 2nd lab

---
+++ lab2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php.lab 1</title>
+</head>
+<body style="display: flex; flex-direction: column; justify-content: space-between; min-height: calc(100vh - 18.4px);">
+    <header>
+        <img style="max-width: 200px; position: absolute;" src="pic/logo.png" alt="#">
+        <p style="margin: 0 auto; padding-top: 20px; width: max-content;">Домашняя работа: Hello, World!</p>
+    </header>
+    <main>
+    <?php
+    $url = 'https://bin.org/post';
+    print_r(get_headers($url));
+    ?>
+    </main>
+    <footer>Работу сделал Веселов Матвей</footer>    
+</body>
+</html>
